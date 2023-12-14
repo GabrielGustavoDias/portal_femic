5f1a9f5e0eb0a02333494f9506fc24ee50d9c823
GabrielGustavoDias <gabrielgdiasb@gmail.com>
2023-12-14 10:37:29 -0300

    Feat: implement new editor text component

--- pages/cursos.tsx
+++ pages/cursos.tsx
@@ -9,31 +9,32 @@ import LayoutBase from '../styles/layout/base';
 export default function Cursos({ courses }: any) {
   return (
     <LayoutBase title="Cursos">
-      {courses.length > 0 && courses.map((course: any) => (
-        <ContainerCourse key={course._id}>
-          <div className="container-image ">
-            <img
-              src={`${baseUrl}/course/banner/${course.banner}`}
-              alt="default image"
-              width={300}
-            />
-            <p>{course.title}</p>
-          </div>
-          <div className="container-info">
-            <p className="strong">{course.title}</p>
-            <p>{course.about}</p>
-            <div className="flex flex-col">
-              <p className="strong">Duração do curso</p>
-              <p className="text-lg font-semibold text-slate-700">
-                {course.time} horas
-              </p>
+      {courses.length > 0 &&
+        courses.map((course: any) => (
+          <ContainerCourse key={course._id}>
+            <div className="container-image ">
+              <img
+                src={`${baseUrl}/course/banner/${course.banner}`}
+                alt="default image"
+                width={300}
+              />
+              <p>{course.title}</p>
             </div>
-            <Link href={`/curso/info/${course._id}`} passHref>
-              <a className="sucess self-end">Ir para curso</a>
-            </Link>
-          </div>
-        </ContainerCourse>
-      ))}
+            <div className="container-info">
+              <p className="strong">{course.title}</p>
+              <div dangerouslySetInnerHTML={{ __html: course.about }} />
+              <div className="flex flex-col">
+                <p className="strong">Duração do curso</p>
+                <p className="text-lg font-semibold text-slate-700">
+                  {course.time} horas
+                </p>
+              </div>
+              <Link href={`/curso/info/${course._id}`} passHref>
+                <a className="sucess self-end">Ir para curso</a>
+              </Link>
+            </div>
+          </ContainerCourse>
+        ))}
     </LayoutBase>
   );
 }
